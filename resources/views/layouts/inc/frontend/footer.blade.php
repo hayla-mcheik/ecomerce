0054f846425ab37256066671be2a5dc7ec5d7823
hayla-mcheik <117450554+hayla-mcheik@users.noreply.github.com>
2024-01-12 14:44:22 +0200

    Initial commit

---
+++ resources/views/layouts/inc/frontend/footer.blade.php
@@ -0,0 +1,21 @@
+<div>
+    <div class="footer-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <h4 class="footer-heading">{{ $appSetting->website_name ?? 'website name' }}</h4>
+                    <div class="footer-underline"></div>
+                    <p>hjgghjk</p>
+</div>
+<div class="col-md-3">
+    <h4 class="footer-heading">ksajdk</h4>
+    <div class="footer-underline"></div>
+    <div class="mb-2"><a href="{{ url('/') }}" class="text-white">Home</a></div>
+    <div class="mb-2"><a href="{{ url('/about-us') }}" class="text-white">Home</a></div>
+    <div class="mb-2"><a href="{{ url('/contact-us') }}" class="text-white">Home</a></div>
+    <div class="mb-2"><a href="{{ url('/blogs') }}" class="text-white">Home</a></div>
+</div>
+</div>
+</div>
+</div>
+</div>
